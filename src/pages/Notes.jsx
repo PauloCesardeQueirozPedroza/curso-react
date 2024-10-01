3f9eddc8c89e5627176ddef5a3a19fb927957500
paulo <paulocesarpedroza2007@gmail.com>
2024-10-01 10:28:57 -0300

    Usando useState

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-import { Container, Button, Modal } from "react-bootstrap";
+import React, {useState} from "react";
+import { Container, Button, Modal, InputGroup, Form} from "react-bootstrap";
 
 const Notes = () => {   
+    const[show, setShow] = useState(false);
+    const [notelist, setNoteList] = useState({
+    })
+    const [note, setNote] = useState({
+        date:'',
+        title:'',
+        description:''
+    })
+    function handleInputChange(event)
+
+    {
+        const { name, Value} = event.target;
+        setNote({
+            ...note,
+            [name]: Value
+        })
+    }
+    
     return(
     <>
         <Container className="p-5 bg-light my-4 rounded-3">
@@ -10,19 +28,44 @@ const Notes = () => {
             <p>
             use essa janela para adicionar anotações e lembretes
             </p>
-            <Button variant="dark">Criar nova anotação</Button>
+            <Button variant="dark" onClick={()=> setShow(true)}>Criar nova anotação</Button>
         </Container>
 
-        <Modal>
+        <Modal show = {show}>
             <Modal.Header>
                 <Modal.Title> Nova anotação </Modal.Title>
             </Modal.Header>
         
         <Modal.Body>
+            <InputGroup>
+            <br/>
+                <InputGroup.Text> Data </InputGroup.Text>
+                    <form.Control
+                    type="date"
+                    Value={note.date}
+            />
+            </InputGroup>
+            <br/>
+
+            <InputGroup>
+            <InputGroup.Text> Titulo </InputGroup.Text>
+                    <form.Control
+                    Value={note.title}
+                    />
+                    </InputGroup>
+
+                    <br/>
+                    <InputGroup>
+                    <InputGroup.Text> Descrição </InputGroup.Text>
+                    <form.Control
+                        as="textarea"
+                        Value={note.description}
+                        />
+                    </InputGroup>
 
         </Modal.Body>
         <Modal.Footer>
-            <Button variant="secondary">Cancelar</Button>
+            <Button variant="secondary" onClick={() => setShow(false)}>Cancelar</Button>
             <Button variant="primary">Adicionar</Button>
         </Modal.Footer>
         
